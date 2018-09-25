aa0971f7308d49357b54fa148d72bbcbbf618df0
Harumi255 <ligia.yamamoto@usp.br>
2018-09-25 09:50:30 -0300

    Segunda modificação

--- app/src/main/java/br/com/dificuldadezero/app/MainActivity.java
+++ app/src/main/java/br/com/dificuldadezero/app/MainActivity.java
@@ -1,5 +1,6 @@
 package br.com.dificuldadezero.app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -48,5 +49,9 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void openMainMenu (View view){
+        Intent intent = new Intent();
     }
 }
