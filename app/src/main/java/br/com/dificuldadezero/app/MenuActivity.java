7efaeb30c9971e8c0cc43672c95827122a19022d
WonK67 <eduardo.campos.dias@usp.br>
2018-10-02 10:29:01 -0300

    agora tela inicial tem relative layout correto

--- app/src/main/java/br/com/dificuldadezero/app/MenuActivity.java
+++ app/src/main/java/br/com/dificuldadezero/app/MenuActivity.java
@@ -1,5 +1,6 @@
 package br.com.dificuldadezero.app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -27,4 +28,18 @@ public class MenuActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    public void openSearchActivity(View view) {
+        Intent intent = new Intent(this, SearchActivity.class);
+        startActivity(intent);
+    }
+
+    public void openInfographActivity(View view) {
+        Intent intent = new Intent(this, InfographActivity.class);
+        startActivity(intent);
+    }
+
+    public void openIndicateActivity(View view) {
+        Intent intent = new Intent(this, IndicateActivity.class);
+        startActivity(intent);
+    }
 }
